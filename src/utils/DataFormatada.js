eb73407cfb73a9bd38267244fe943bdc480cb743
Mariana Alvares <mariana11areal@hotmail.com>
2022-03-15 18:20:16 -0300

    Melhoria de comparação

--- src/utils/DataFormatada.js
+++ src/utils/DataFormatada.js
@@ -1,9 +1,9 @@
 export function dataAtualFormatada() {
   var data = new Date();
   var dia = data.getDate().toString();
-  var diaF = dia.length == 1 ? "0" + dia : dia;
+  var diaF = dia.length === 1 ? "0" + dia : dia;
   var mes = (data.getMonth() + 1).toString();
-  var mesF = mes.length == 1 ? "0" + mes : mes;
+  var mesF = mes.length === 1 ? "0" + mes : mes;
   var anoF = data.getFullYear();
   var hora = data.getHours();
   var minuto = data.getMinutes() < 10 ? "0" + data.getMinutes() : data.getMinutes();
